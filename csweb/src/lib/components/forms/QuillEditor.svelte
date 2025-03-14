878edef49bfe43b0f7790d0f5381bbed53c634c2
cscoding21 <jeph@cscoding.io>
2025-03-14 08:32:55 -0700

    fix: added mentions to chat front end

--- csweb/src/lib/components/forms/QuillEditor.svelte
+++ csweb/src/lib/components/forms/QuillEditor.svelte
@@ -2,6 +2,7 @@
     import { onMount } from "svelte"
     import Quill from 'quill';
     import type { Delta } from "quill/core";
+    import type { User } from "$lib/graphql/generated/sdk";
     import { normalizeID } from "$lib/utils/id";
 
     import 'quill/dist/quill.core.css'
@@ -10,17 +11,19 @@
     import "quill-mention/autoregister";
 
     import { handleFileUpload } from "$lib/services/file";
+	import { findAllUsers } from "$lib/services/user";
 	
-
     interface Props {
         contents:any;
         attachContext: string;
         quillEditor: any;
+        userOpts?: User[]
     }
     let { 
         contents = $bindable(), 
         attachContext, 
-        quillEditor = $bindable()
+        quillEditor = $bindable(),
+        userOpts
     }:Props = $props();
 
     let quill : Quill;
@@ -55,17 +58,18 @@
 	
     onMount(async () => {
         // debug: 'log',
-        //mention: mentionOpts
+        
         quill = new Quill("#" + editorID, {
             theme: "bubble",
             modules: {
+                mention: mentionOpts,
                 toolbar: toolbarOptions,
                 
             }
         });
 
         if (contents) {
-            console.log("content", contents)
+            console.log("content", contents.data)
             quill.setContents(JSON.parse(contents), "user");
         }
 
@@ -77,21 +81,17 @@
         quill.focus()
     });
 
-    let atValues = [
-        { id: 'jdoe@its-global.vn', value: 'John Doe' },
-        { id: 'jadoe@its-global.vn', value: 'Jane Doe' },
-        { id: 'jdmith@its-global.vn', value: 'John Smith' },
-        { id: 'jasmith@its-global.vn', value: 'Jane Smith' },
-    ]
+    let atValues = $state([{id: "", value: ""}])
 
+    //  mentionContainerClass: "w-64 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-1/3 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500",
+    //  listItemClass: "block mb-2  py-2 text-sm font-medium text-gray-900 dark:text-white cursor-pointer",
+    //  mentionListClass: "block mb-2 text-sm font-medium text-gray-900 dark:text-white cursor-pointer",
+
+    // selectKeys: [13,9],
     const mentionOpts = {
             allowedChars: /^[A-Za-z\sÅÄÖåäö]*$/,
             mentionDenotationChars: ["@"],
-            mentionContainerClass: "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-1/3 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500",
-            listItemClass: "block mb-2 text-sm font-medium text-gray-900 dark:text-white cursor-pointer",
-            mentionListClass: "block mb-2 text-sm font-medium text-gray-900 dark:text-white cursor-pointer",
             minChars: 1,
-            selectKeys: [9],
             source: function(searchTerm:any, renderList:any, mentionChar:any) {
                 let values = atValues;
 
@@ -113,11 +113,25 @@
                 }
             }
         }
+
+    const loadPage = () => {
+        findAllUsers().then(u => {
+            atValues = u.results?.map(u => { return  { id: u.email, value: u.firstName + ' ' + u.lastName } } ) as []
+        })
+    }
 </script>
 
 
-<!-- bg-gray-50 border border-gray-200 dark:bg-gray-700 dark:border-gray-600 -->
+{#await loadPage()}
+    Loading...
+    <span title="mentionContainerClass" class="w-64 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"></span>
+    <span title="listItemClass" class="block mb-2 py-2 text-sm font-medium text-gray-900 dark:text-white cursor-pointer"></span>
+    <span title="mentionListClass" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white cursor-pointer"></span>
+    <span title="scratch" class="text-gray-600"></span>
+{:then promiseData} 
 <div
     class="w-full rounded-lg text-gray-100">
     <div id="{editorID}"></div>
 </div>
+
+{/await}
