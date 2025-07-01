56d6e1bae1f9bd97930cd13a0d955f9a68f8208b
cscoding21 <jeph@csoding.io>
2025-07-01 13:05:22 -0700

    fix: pubsub checkpoint

--- csserver/cmd/csprocessor/main.go
+++ csserver/cmd/csprocessor/main.go
@@ -1,17 +1,49 @@
 package main
 
 import (
+	"context"
+	"csserver/internal/appserv/csmap"
+	"csserver/internal/config"
 	"fmt"
 	"time"
 
+	"github.com/nats-io/nats.go"
+	"github.com/nats-io/nats.go/jetstream"
 	log "github.com/sirupsen/logrus"
 )
+
+// setup global configuration values
+func init() {
+	config.InitConfig()
+	config.InitLogger()
+
+	csmap.ClearAllCaches()
+}
 
 func main() {
 	log.Info("Processor started")
 
-	go forever()
-	select {}
+	// connect to nats server
+	nc, _ := nats.Connect(config.Config.PubSub.Host)
+
+	// create jetstream context from nats connection
+	js, _ := jetstream.New(nc)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	// get existing stream handle
+	stream, _ := js.Stream(ctx, "foo")
+
+	// retrieve consumer handle from a stream
+	cons, _ := stream.Consumer(ctx, "cons")
+
+	// consume messages from the consumer in callback
+	cc, _ := cons.Consume(func(msg jetstream.Msg) {
+		fmt.Println("Received jetstream message: ", string(msg.Data()))
+		msg.Ack()
+	})
+	defer cc.Stop()
 }
 
 func forever() {
